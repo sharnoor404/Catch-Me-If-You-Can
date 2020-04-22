8f8f8fa44205a62ec30aa4bea008742de307010e
sharnoor404 <sharnoor404@gmail.com>
2020-04-22 13:34:26 +0530

    increase score functionality added

--- app/src/main/java/com/example/catchmeifyoucan/MainActivity.kt
+++ app/src/main/java/com/example/catchmeifyoucan/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.catchmeifyoucan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var score:Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        score=0
+
+    }
+
+    fun increaseScore(view:View){
+        score++
+        scoreText.text="Score: $score"
+
     }
 }
