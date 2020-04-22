5362d10c5d0e00c81c1b89146c7eb9e1dcb55a19
sharnoor404 <sharnoor404@gmail.com>
2020-04-22 18:53:19 +0530

    hide images functionality added

--- app/src/main/java/com/example/catchmeifyoucan/MainActivity.kt
+++ app/src/main/java/com/example/catchmeifyoucan/MainActivity.kt
@@ -2,23 +2,70 @@ package com.example.catchmeifyoucan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StrictMath.random
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     var score:Int=0
+    var imageArray=ArrayList<ImageView>()
+
+    var handler : Handler= Handler()
+    var runnable: Runnable= Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         score=0
 
+        imageArray= arrayListOf(imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9,imageView10)
+        hideImages()
+
+        object: CountDownTimer(10000,1000){
+            override fun onFinish() {
+                timeText.text="TIME UP!"
+
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeText.text="Time: " + millisUntilFinished/1000
+            }
+
+        }.start()
     }
 
-    fun increaseScore(view:View){
+    fun hideImages(){
+
+        runnable= object : Runnable {
+            override fun run() {
+
+                for(image in imageArray){
+                    image.visibility=View.INVISIBLE
+                }
+
+                val random= java.util.Random()
+                val index=random.nextInt(8-0)
+                imageArray[index].visibility=View.VISIBLE
+
+                handler.postDelayed(runnable,500)
+            }
+
+        }
+        handler.post(runnable)
+
+    }
+
+
+    fun increaseScore(view:View) {
         score++
-        scoreText.text="Score: $score"
-
+        scoreText.text = "Score: $score"
     }
+
+
+
 }
